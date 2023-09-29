9257630bef9bf51d49b1b868ad03fe86605ea5c8
Cecileaudree <audreednca@gmail.com>
2023-09-29 15:22:26 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mboa_Culture',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
